8550a539f45eaf7615c466430142a48241c803b3
MohanG <mohanaskcg@gmail.com>
2022-08-29 13:18:32 +0800

    Feature file commit ToDoPage pageobjects class commit

---
+++ src/main/java/pages/ToDoPage.java
@@ -0,0 +1,165 @@
+package pages;
+
+import io.cucumber.datatable.DataTable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utils.BaseDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static utils.LoadProp.getProperty;
+
+ public class ToDoPage extends BaseDriver {
+
+    // Page Element Locators
+    By header=By.xpath("//header/h1");
+    By inputField=By.className("new-todo");
+    By footer=By.className("info");
+    By listItems=By.xpath("//ul[@class='todo-list']/li");
+    By unToggledItem=By.xpath("//li[@class='todo']");
+    By itemValue=By.xpath("//label");
+    By toggledItem=By.xpath("//li[@class='todo completed']");
+    By toggleItem=By.xpath("./preceding-sibling::input[@class='toggle']");
+    By removeItem=By.xpath("./following-sibling::button[@class='destroy']");
+    By toggleAll=By.xpath("//label[@for='toggle-all']");
+    By itemFooterCount=By.className("todo-count");
+    By clearCompleted=By.className("clear-completed");
+
+
+    List<String> unToggledItemsList=new ArrayList<>();
+    List<String> toggledItemsList=new ArrayList<>();
+
+    public void getToDoPage() {
+        driver.get(getProperty("url"));
+    }
+
+     public boolean isHeaderDisplayed(String headerText) {
+        return driver.findElement(header).getText().equalsIgnoreCase(headerText);
+     }
+
+     public boolean isPlaceHolderTextDisplayed(String placeHolderText) {
+         return driver.findElement(inputField).getAttribute("placeholder").equalsIgnoreCase(placeHolderText);
+     }
+
+     public boolean isFooterDisplayed(){
+        return driver.findElement(footer).isDisplayed();
+     }
+
+
+     public boolean checkItemCount() {
+        long itemCount= driver.findElements(unToggledItem).stream().filter(item -> !item.getAttribute("class").contains("completed")).count();
+         return driver.findElement(itemFooterCount).getText().contains(String.valueOf(itemCount));
+     }
+
+     public void enterItem(String unToggledItem) {
+        driver.findElement(inputField).sendKeys(unToggledItem+ Keys.ENTER);
+         unToggledItemsList.add(unToggledItem);
+     }
+
+     public void enterMultipleItems(DataTable table){
+         List<List<String>> tableRow=table.asLists(String.class);
+         tableRow.forEach(column->enterItem(column.get(0)));
+     }
+
+
+     public boolean isItemsInList(String toggleType) {
+         boolean itemFlag=true;
+         if(toggleType.equalsIgnoreCase("untoggled")){
+         List<WebElement> toDoList=driver.findElements(unToggledItem);
+             for (WebElement listItem:toDoList) {
+                 if (!unToggledItemsList.stream().filter(unToggledItem -> unToggledItem.contains(listItem.getText())).findAny().isPresent()) {
+                     itemFlag = false;
+                     break;
+                 }
+             }
+         }
+         if(toggleType.equalsIgnoreCase("toggled")){
+             List<WebElement> toDoList=driver.findElements(toggledItem);
+             for (WebElement listItem:toDoList) {
+                 if (!toggledItemsList.stream().filter(unToggledItem -> unToggledItem.contains(listItem.getText())).findAny().isPresent()) {
+                     itemFlag = false;
+                     break;
+                 }
+             }
+         }
+        return itemFlag;
+     }
+
+     public void clickTab(String tabText) {
+        driver.findElement(By.linkText(tabText)).click();
+     }
+
+     public boolean noItemsDisplayed(String toggleType) {
+        if(toggleType.equalsIgnoreCase("toggled")) {
+            return !(driver.findElements(unToggledItem).size() > 0);
+        }
+        else{
+            return !(driver.findElements(toggledItem).size() > 0);
+        }
+     }
+
+     public void toggleItem(String itemName) {
+        List<WebElement> items=driver.findElements(By.xpath((unToggledItem.toString()
+                +itemValue.toString()).replaceAll("By.xpath: ","")));
+         for (WebElement item:items) {
+             if(item.getText().equalsIgnoreCase(itemName)){
+                 item.findElement(toggleItem).click();
+             }
+         }
+         unToggledItemsList.removeIf(item->item.equalsIgnoreCase(itemName));
+         toggledItemsList.add(itemName);
+     }
+
+     public void clickToggleAll() {
+        driver.findElement(toggleAll).click();
+     }
+
+     public boolean isItemsToggled() {
+        boolean toggleFlag=true;
+        List<WebElement> items=driver.findElements(listItems);
+         for (WebElement item:items) {
+             if(!item.getAttribute("class").contains("completed")){
+                 toggleFlag=false;
+                 break;
+             }
+         }
+     return toggleFlag;
+     }
+
+     public boolean isClearBtnDisplayed() {
+        return driver.findElement(clearCompleted).isDisplayed();
+     }
+
+     public void clickClearButton() {
+        driver.findElement(clearCompleted).click();
+     }
+
+     public void removeItem(String itemName) {
+         List<WebElement> items=driver.findElements(By.xpath((listItems.toString()
+                 +itemValue.toString()).replaceAll("By.xpath: ","")));
+         for (WebElement item:items) {
+             if(item.getText().equalsIgnoreCase(itemName)){
+                 JavascriptExecutor js=(JavascriptExecutor)driver;
+                 js.executeScript("arguments[0].click();",item.findElement(removeItem));
+             }
+         }
+         unToggledItemsList.removeIf(item->item.equalsIgnoreCase(itemName));
+         toggledItemsList.removeIf(item->item.equalsIgnoreCase(itemName));
+     }
+
+     public boolean isItemDisplayed(String itemName) {
+        boolean itemFlag=false;
+         List<WebElement> items=driver.findElements(By.xpath((listItems.toString()
+                 +itemValue.toString()).replaceAll("By.xpath: ","")));
+         for (WebElement item:items) {
+             if(item.getText().equalsIgnoreCase(itemName)){
+                 itemFlag=true;
+                 break;
+             }
+         }
+         return itemFlag;
+     }
+ }
---
+++ src/test/java/features/ToDoListActions.feature
@@ -0,0 +1,130 @@
+Feature: In order to remember the things I want to do, as a ToDo MVC user, I want to manage my todo list
+
+  #1
+  Scenario Outline: Verify the static content on ToDos homepage
+    Given user is on the To Do List home page
+    Then user should view <headerText> header text in To Do page
+    And user should view <placeHolderText> place holder text in To Do field
+    And user should view footer below the To Do form
+
+    Examples:
+      | headerText  | placeHolderText |
+      |todos| What needs to be done?  |
+  #2
+  Scenario Outline: verify if entered item is displayed in to do list
+    Given user is on the To Do List home page
+    When user enters <item> in To Do field
+    Then user should view untoggled items in the list
+    And the item count should be updated in the footer
+
+    Examples:
+      | item  |
+      |pickup groceries|
+
+  @ToDoListActions
+  #3
+  Scenario Outline: verify if numeric and special characters entered are stored in list
+    Given user is on the To Do List home page
+    When user enters <item> in To Do field
+    Then user should view untoggled items in the list
+    And the item count should be updated in the footer
+
+    Examples:
+      | item  |
+      |####12*^+23424|
+
+  #4
+  Scenario: verify if un-toggled item is displayed in All and Active tabs
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+    |pickup groceries|
+    |buy fruits|
+    |watch football|
+    Then user should view untoggled items in the list
+    And the item count should be updated in the footer
+    When user clicks on Active tab in footer
+    Then user should view untoggled items in the list
+
+  #5
+  Scenario: verify if un-toggled item is displayed in Completed tabs
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+      |pickup groceries|
+      |buy fruits|
+      |watch football|
+    Then user should view untoggled items in the list
+    And the item count should be updated in the footer
+    When user clicks on Completed tab in footer
+    Then user should not view any untoggled items in the list
+
+  #6
+  Scenario: verify if toggled item is displayed in Active tab
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+      |pickup groceries|
+      |buy fruits|
+      |watch football|
+    Then user should view untoggled items in the list
+    When user toggles buy fruits item in To Do list
+    Then the item count should be updated in the footer
+    When user clicks on Active tab in footer
+    Then user should view untoggled items in the list
+
+  #7
+  Scenario: verify if toggled item is displayed in Completed tab
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+      |pickup groceries|
+      |buy fruits|
+      |watch football|
+    Then user should view untoggled items in the list
+    When user toggles buy fruits item in To Do list
+    Then the item count should be updated in the footer
+    When user clicks on Completed tab in footer
+    Then user should view toggled items in the list
+
+  #8
+  Scenario: Verify if all items are marked as completed when clicked on toggle all button
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+      |pickup groceries|
+      |buy fruits|
+      |watch football|
+    Then user should view untoggled items in the list
+    When user clicks on toggle all button
+    Then user should view all items marked as completed
+
+  #9
+  Scenario: Verify if Clear completed button is displayed when an item is toggled
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+      |pickup groceries|
+      |buy fruits|
+      |watch football|
+    Then user should view untoggled items in the list
+    When user toggles buy fruits item in To Do list
+    Then user should view Clear completed button in To Do list
+
+  #10
+  Scenario: Verify if toggled items are removed from list when clicked on clear completed button
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+      |pickup groceries|
+      |buy fruits|
+      |watch football|
+    Then user should view untoggled items in the list
+    When user toggles buy fruits item in To Do list
+    Then user should view Clear completed button in To Do list
+    When user clicks on Clear completed button
+    Then user should not view any toggled items in the list
+
+  #11
+  Scenario: Verify if items are removed from list when clicked on cross button of item
+    Given user is on the To Do List home page
+    When user enters multiple items to To Do List
+      |pickup groceries|
+      |buy fruits|
+      |watch football|
+    Then user should view untoggled items in the list
+    When user clicks on cross button of item buy fruits
+    Then user should not view buy fruits item in the list
